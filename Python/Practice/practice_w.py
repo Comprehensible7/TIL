98d0a1bfe622694f1148e98cc039fc8a4d5ea2e3
Comprehensible7 <mltr0126@gmail.com>
2020-10-30 23:50:11 +0900

    Revert "TIL"
    This reverts commit 15e3baf1ac8136a606c5420bb88542edab964f26.

--- Python/Practice/practice_w.py
+++
@@ -1,7 +0,0 @@
-print('풍선')
-
-print("나비")
-
-print('zzzzzzzzzzz')
-
-print('z'*9)
--- Python/Practice/quiz2.py
+++
@@ -1,7 +0,0 @@
-from random import *
-
-# 숫자 범위 지정
-date = randint(4, 28)
-
-# date는 숫자이기때문에 프린트문에서 출력하기 위해 str로 변환해줌
-print('오프라인 스터디 모임 날짜는 매월' + str(date) + ' 일로 선정되었습니다.')
